602c3be4b4b61f9c36dffdcd167392765aadc357
Luke Boggs <lukeboggs7@gmail.com>
2023-11-13 15:23:16 +0700

    Add new function for authorization

---
+++ authorize/index.js
@@ -0,0 +1,6 @@
+import { http } from '@google-cloud/functions-framework';
+import querystring from 'querystring';
+
+http('authorize', async (req, res) => {
+  res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify(req.query)}`);
+});
