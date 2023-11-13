2131c02deb5728e330df981efaa16e6f99b04d8c
Luke Boggs <lukeboggs7@gmail.com>
2023-11-13 12:08:43 +0700

    Fetch profile id for a logged in spotify user

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-exports.helloWorld = (req, res) => {
-    res.send('Hello, World');
-};
+import { http } from '@google-cloud/functions-framework';
+import axios from 'axios';
+
+http('createPlaylist', async (req, res) => {
+  const SPOTIFY_BASE_URL = 'https://api.spotify.com/v1';
+  console.log(req.headers.authorization);
+
+  const accessToken = req.headers.authorization;
+
+  const profileResponse = await axios.get(
+    `${SPOTIFY_BASE_URL}/me`,
+    { headers: { Authorization: `Bearer ${accessToken}` } }
+  );
+
+  console.log('------------profileResponse----------------');
+  console.log(profileResponse);
+
+  const profileId = profileResponse.data.id;
+
+  res.send(`Hello, ${profileId}`);
+});
